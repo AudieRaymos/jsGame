6747687f56442043768bfcd042c7e17b44dade9b
Audie Raymos <audriannakraymos@gmail.com>
2022-02-11 15:40:16 -0800

    onclick so game can start

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function init() {
     document.getElementById("newGameButton").style.display = "none";
     document.getElementById("gameArea").style.display = "none";
 }
+
+function startGameView() {
+    document.getElementById('welcomeScreen').style.display = "none";
+    document.getElementById('gameArea').style.display = "block";
+}
+
+function easyMode() {
+    startGameView();
+}
+
+function hardMode() {
+    startGameView();
+}
